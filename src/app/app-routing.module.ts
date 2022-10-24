7119b711c562abe0e7a52170907416b5fbb72454
Gonzalo Ariel Gomez <gonza12ariel@gmail.com>
2022-10-23 23:37:14 -0300

    Iniciando proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentListComponent } from './component-list/component-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component : ComponentListComponent,
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
